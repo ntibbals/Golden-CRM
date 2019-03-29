63b17b31823df005bffb3565d499221879a69e23
ntibbals <ntibbals@gmail.com>
2019-03-29 12:53:58 -0700

    setup initial build out

---
+++ Golden-CRM/Golden-CRM/Data/GoldenDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Golden_CRM.Data
+{
+    public class GoldenDbContext : DbContext
+    {
+
+        public GoldenDbContext(DbContextOptions<GoldenDbContext> options): base(options)
+        {
+
+        }
+    }
+}
