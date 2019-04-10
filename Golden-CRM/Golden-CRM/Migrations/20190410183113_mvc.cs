a8f7922ca9c95697d6f8390d1f4a1838f723cf1a
ntibbals <ntibbals@gmail.com>
2019-04-10 12:11:06 -0700

    seeded data and updated links

---
+++ Golden-CRM/Golden-CRM/Migrations/20190410183113_mvc.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Golden_CRM.Migrations
+{
+    public partial class mvc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserID",
+                table: "Notes",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_CustomerID",
+                table: "Notes",
+                column: "CustomerID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Customers_CustomerID",
+                table: "Notes",
+                column: "CustomerID",
+                principalTable: "Customers",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Customers_CustomerID",
+                table: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notes_CustomerID",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Notes");
+        }
+    }
+}
---
+++ Golden-CRM/Golden-CRM/Migrations/20190410184438_seed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Golden_CRM.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "ID", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { 1, "jd@doe.com", "John", "Doe", "555-555-5555" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "ID",
+                keyValue: 1);
+        }
+    }
+}
