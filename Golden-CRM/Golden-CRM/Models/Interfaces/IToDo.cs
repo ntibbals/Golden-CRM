25992ab40a9557d41098f2111ddb5844d41be3f3
ntibbals <ntibbals@gmail.com>
2019-05-10 15:24:21 -0700

    working on tasks/todos

---
+++ Golden-CRM/Golden-CRM/Models/Interfaces/IToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Golden_CRM.Models.Interfaces
+{
+    public interface IToDo
+    {
+        Task<Note> FindToDo(int id);
+        Task<List<Note>> FindToDos(int customerID);
+        Task<List<Note>> GetToDos();
+        Task UpdateToDo(ToDo toDo);
+        Task SaveAsync(ToDo toDo);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Golden-CRM/Golden-CRM/Models/ToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Golden_CRM.Models
+{
+    public class ToDo
+    {
+        public int ID { get; set; }
+        public int CustomerID { get; set; }
+        public string UserID { get; set; }
+        public string AssignedOwner { get; set; }
+        public ContactTypes ContactType { get; set; }
+        [Column(TypeName = "varchar(max)")]
+        [MaxLength]
+        public string Comment { get; set; }
+        public DateTime DueDate { get; set; }
+
+    }
+
+}
